0289a070b6187996dc11e00376ce5f9ec492594d
yangml <y787093482@126.com>
2019-01-18 10:07:24 +0800

    update modify

---
+++ git.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "update modify"
+git remote add origin https://github.com/yangmingleiSYSU/Bioinformatic_homework.git
+git push -u origin master
+git push -u origin master
+
